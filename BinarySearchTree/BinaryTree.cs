5a43ba376c42d3808acb1029b94fb4000c6da06c
Scott Tracy <scott.tracy.st@gmail.com>
2018-06-20 14:05:56 -0500

    created and added basic binary tree structure

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        private Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+
+        public void CreateTree()
+        {
+            root = new Node(50);
+            root.leftChild = new Node(25);
+            root.rightChild = new Node(75);
+            root.leftChild.leftChild = new Node(19);
+            root.leftChild.rightChild = new Node(39);
+            root.rightChild.leftChild = new Node(69);
+            root.rightChild.rightChild = new Node(89);
+        }
+    }
+}
---
+++ BinarySearchTree/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinarySearchTree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinarySearchTree")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6eaf27d-c496-4308-85cf-836ff8929a17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
